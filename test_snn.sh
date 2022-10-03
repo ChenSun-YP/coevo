d58bb1139f111cba4aaa273f33a94e4e49804235
Chen Sun <chensun@beluga3.int.ets1.calculquebec.ca>
2022-10-03 17:40:12 -0400

    good

--- test_snn.sh
+++ test_snn.sh
@@ -10,13 +10,17 @@
 module load python/3.8
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
-pip --version
-pip install --no-index torch torchvision==0.9.1 torchtext torchaudio
-pip install thop
-pip install tensorboard
+pip3 --version
+pip3 install --no-index torch torchvision==0.9.1 torchtext torchaudio
+
+pip3 install thop
+pip3 install --no-index torchsummary
+pip3 install --no-index  tensorboard
+git clone https://github.com/fangwei123456/spikingjelly.git
+
 nvidia-smi                        # you can use 'nvidia-smi' for a test
 # ResNet56 on CIFAR-10
- python3 main_snn.py --dataset cifar10 --arch vgg  \
+ python  main_snn.py --dataset cifar10 --arch vgg  \
                   --ft_epoch 100 --lr_milestone 50 \
                   --dict_path ./models/vgg16.th  --pop_init_rate 0.95  \
                   --prune_limitation 0.90 --batch-size 128 --valid_ratio 0.8  \
